270b077cd9b152ff06c619d1c6270f684dfa3931
delynlpy@gmail.com <delynlpy@gmail.com>
2024-05-11 21:56:16 +0800

    initial commit

---
+++ src/utilities/users-api.js
@@ -0,0 +1,21 @@
+import debug from "debug";
+
+// This is the base path of the Express route we'll define
+const BASE_URL = "/api/users";
+const log = debug("mern:utilities:users-api");
+
+export async function signUp(userData) {
+  log("userData: %o", userData);
+  const res = await fetch(BASE_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(userData),
+  });
+
+  if (res.ok) {
+    return res.json();
+  } else {
+    log("error: Invalid Sign Up");
+    throw new Error("Invalid Sign Up");
+  }
+}
