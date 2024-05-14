4a52119d0ada3ecdf6fe54dbb475aeb75781c41e
Kavin <kavin.ned@gmail.com>
2024-05-14 21:34:07 +0800

    feat: create navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,20 +2,20 @@ import { Link, NavLink } from "react-router-dom";
 import { logOut } from "../../utilities/users-service";
 
 export default function NavBar({ setUser }) {
-  const handleLogOut = () => {
-    logOut();
-    setUser(null);
-  };
+	const handleLogOut = () => {
+		logOut();
+		setUser(null);
+	};
 
-  return (
-    <nav>
-      <NavLink to="/orders">Order History</NavLink>
-      &nbsp; | &nbsp;
-      <NavLink to="/orders/new">New Order</NavLink>
-      &nbsp;&nbsp;
-      <Link to="" onClick={handleLogOut}>
-        Log Out
-      </Link>
-    </nav>
-  );
+	return (
+		<nav>
+			<NavLink to="/">Home</NavLink>
+			&nbsp; | &nbsp;
+			<NavLink to="/events/:userID">My Events</NavLink>
+			&nbsp;&nbsp;
+			<Link to="" onClick={handleLogOut}>
+				Log Out
+			</Link>
+		</nav>
+	);
 }
