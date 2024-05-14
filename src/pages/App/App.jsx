ee6b6684fd345e8a06587cc1a73b6e99a6242df9
Kavin <kavin.ned@gmail.com>
2024-05-14 21:33:45 +0800

    feat: create routes

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,43 +1,38 @@
 import debug from "debug";
 import { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import NavBar from "../../components/NavBar/NavBar";
 import { getUser } from "../../utilities/users-service";
-import AuthPage from "../AuthPage/AuthPage";
-import NewOrderPage from "../NewOrderPage/NewOrderPage";
-import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
+// import AuthPage from "../AuthPage/AuthPage";
+import CreateEventPage from "../CreateEventPage/CreateEventPage";
+import EventSetupPage from "../CreateEventPage/EventSetupPage/EventSetupPage";
+import TaskSetupPage from "../CreateEventPage/TaskSetupPage/TaskSetupPage";
 
 const log = debug("mern:pages:App:App");
 
 function App() {
-  const [user, setUser] = useState(getUser());
-  log("user %o", user);
+	const [user, setUser] = useState(getUser());
+	log("user %o", user);
 
-  if (!user) {
-    return (
-      <main className="App">
-        <AuthPage setUser={setUser} />
-      </main>
-    );
-  }
+	// if (!user) {
+	// 	return (
+	// 		<main className="App">
+	// 			<AuthPage setUser={setUser} />
+	// 		</main>
+	// 	);
+	// }
 
-  return (
-    <>
-      <main className="App">
-        <NavBar setUser={setUser} />
-
-        <Routes>
-          <Route path="/orders" element={<OrderHistoryPage />} />
-          <Route path="/orders/new" element={<NewOrderPage />} />
-
-          <Route path="/orders2" element={<OrderHistoryPage />}>
-            <Route path="new" element={<NewOrderPage />} />
-            <Route path="simon" element={<p>Simon</p>} />
-          </Route>
-        </Routes>
-      </main>
-    </>
-  );
+	return (
+		<main className="App">
+			<NavBar setUser={setUser} />
+			<Link to="/event/new">Create new event</Link>
+			<Routes>
+				<Route path="/event/new" element={<CreateEventPage />} />
+				<Route path="/event/new/details" element={<EventSetupPage />} />
+				<Route path="/event/:eventID/task/new" element={<TaskSetupPage />} />
+			</Routes>
+		</main>
+	);
 }
 
 export default App;
