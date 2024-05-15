172341ed853afb4f35bf8133019d91fb4054d5ad
zonghan <xzonghanx@gmail.com>
2024-05-15 17:52:45 +0800

    feat: Update & Delete events&tasks

--- controllers/api/eventsController.js
+++ controllers/api/eventsController.js
@@ -27,13 +27,18 @@ const create = async (req, res) => {
 };
 
 const userIndex = async (req, res) => {
-	const eventsHosted = await Event.find({ host: userId });
-	const eventsAttending = await Event.find({ attendees: userId });
-	const events = { eventsHosted, eventsAttending };
-	debug("events %o:", events);
-	res.status(200).json(events);
+	try {
+		const eventsHosted = await Event.find({ host: userId });
+		const eventsAttending = await Event.find({ attendees: userId });
+		const events = { eventsHosted, eventsAttending };
+		debug("events %o:", events);
+		res.status(200).json(events);
+	} catch (error) {
+		res.status(500).json({ error });
+	}
 };
 
+//TODO get userID from body.
 const join = async (req, res) => {
 	try {
 		const event = await Event.findById(req.params.eventId);
@@ -41,15 +46,46 @@ const join = async (req, res) => {
 			return res.status(404).json({ error: "event not found" });
 		}
 		if (event.attendees.includes(userId2)) {
-			//TODO change this to toggle push or pop
-			//TODO change this to follow CRUD convention
-			return res.status(400).json({ error: "user is already attending event" });
+			event.attendees.pop(userId2);
+			await event.save();
+			return res.status(200).json(event);
 		}
 		event.attendees.push(userId2);
 		await event.save();
-		res.status(201).json(event);
+		res.status(200).json(event);
 	} catch (error) {
 		res.status(500).json({ error });
+	}
+};
+
+const edit = async (req, res) => {
+	try {
+		// more efficient, but less flexibility. also need to set {new:true, runValidators:true}
+		// const event = await Event.findByIdAndUpdate(req.params.eventId , req.body);
+		const event = await Event.findById(req.params.eventId);
+		if (!event) {
+			return res.status(404).json({ error: "event not found" });
+		}
+		const { name, location, datetime } = req.body;
+		event.name = name;
+		event.location = location;
+		event.datetime = datetime;
+		await event.save();
+		res.status(200).json(event);
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+};
+
+const deleteOne = async (req, res) => {
+	try {
+		const event = await Event.findByIdAndDelete(req.params.eventId);
+		if (!event) {
+			return res.status(404).json({ error: "Event not found" });
+		}
+		res.status(200).json(event);
+	} catch (error) {
+		res.status(500).json({ error: "Error deleting event" });
 	}
 };
 
@@ -58,4 +94,6 @@ module.exports = {
 	index,
 	userIndex,
 	join,
+	edit,
+	deleteOne,
 };
--- controllers/api/tasksController.js
+++ controllers/api/tasksController.js
@@ -22,7 +22,38 @@ const create = async (req, res) => {
 	}
 };
 
+const edit = async (req, res) => {
+	try {
+		// more efficient, but less flexibility. also need to set {new:true, runValidators:true}
+		// const task = await Task.findByIdAndUpdate(req.params.taskId, req.body);
+		const task = await Task.findById(req.params.taskId);
+		debug("task %o:", task);
+		const { name, status, delegated } = req.body;
+		task.name = name;
+		task.status = status;
+		task.delegated = delegated;
+		await task.save();
+		res.status(200).json(task);
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+};
+
+const deleteOne = async (req, res) => {
+	try {
+		const task = await Task.findByIdAndDelete(req.params.taskId);
+		if (!task) {
+			return res.status(404).json({ error: "Task not found" });
+		}
+		res.status(200).json(task);
+	} catch (error) {
+		res.status(500).json({ error: "Error deleting task" });
+	}
+};
+
 module.exports = {
 	create,
 	index,
+	edit,
+	deleteOne,
 };
