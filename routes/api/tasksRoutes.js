89435b5c5b75f79c1d919b8e0bd5389cfc80094a
zonghan <xzonghanx@gmail.com>
2024-05-15 22:13:15 +0800

    cleanup

--- routes/api/tasksRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { create } = require("../../controllers/api/tasksController");
-
-router.post("/:eventId/tasks", create);
-module.exports = router;
