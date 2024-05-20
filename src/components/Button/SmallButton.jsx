68c2d3ab8d22229828407d53ae0106542518cd2f
Kavin <kavin.ned@gmail.com>
2024-05-20 17:47:25 +0800

    feat: set disable property for btns

--- src/components/Button/SmallButton.jsx
+++ src/components/Button/SmallButton.jsx
@@ -1,9 +1,10 @@
-function SmallButton({ children, onClick, type }) {
+function SmallButton({ children, onClick, type, disabled }) {
 	return (
 		<button
-			className="transition-all drop-shadow-lg bg-white border-2 border-transparent rounded-md hover:border-slate-300 hover:rounded-lg active:bg-slate-300 focus-visible:bg-slate-300 shadow-inner cursor-pointer text-center font-medium text-slate-700 text-xs"
+			className="transition-all drop-shadow-lg bg-white border-2 border-transparent rounded-md hover:border-slate-300 hover:rounded-lg active:bg-slate-300 focus-visible:bg-slate-300 shadow-inner cursor-pointer text-center font-medium text-slate-700 text-xs disabled:cursor-not-allowed disabled:opacity-50"
 			onClick={onClick}
 			type={type}
+			disabled={disabled}
 		>
 			{children}
 		</button>
